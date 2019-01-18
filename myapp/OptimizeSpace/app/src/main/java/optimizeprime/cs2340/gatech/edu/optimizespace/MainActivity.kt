6598b1c09f9eff0bfd708729e2e33da1d4927710
jpham197 <jpham197@gmail.com>
2019-01-18 14:23:29 -0500

    initial android project

---
+++ myapp/OptimizeSpace/app/src/main/java/optimizeprime/cs2340/gatech/edu/optimizespace/MainActivity.kt
@@ -0,0 +1,12 @@
+package optimizeprime.cs2340.gatech.edu.optimizespace
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
