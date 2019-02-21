a5f757340a6f8bb77fb0c8c2d5742bad45dda974
jpham197 <jpham197@gmail.com>
2019-02-21 17:33:46 -0500

    logcat and connected view to model

--- Android/app/src/main/java/com/example/android/MainActivity.java
+++ Android/app/src/main/java/com/example/android/MainActivity.java
@@ -6,8 +6,19 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.view.View.OnClickListener;
+import android.util.Log;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +28,36 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         String name = "";
+
+
+        String[] arraySpinner = new String[] {
+                "Easy", "Normal", "Hard", "Extreme"
+        };
+        Spinner s = (Spinner) findViewById(R.id.spinner);
+
+        EditText playerName = (EditText) findViewById(R.id.inputPlayerName);
+        TextView skillPilotLevel = (TextView) findViewById(R.id.skillPilotLevel);
+        TextView skillFighterLevel = (TextView) findViewById(R.id.skillFighterLevel);
+        TextView skillEngineerLevel = (TextView) findViewById(R.id.skillEngineerLevel);
+        TextView skillTraderLevel = (TextView) findViewById(R.id.skillTraderLevel);
+
+        final String playerInformation = "Player Name: " + playerName.getText().toString()
+                + "\nPilot Skill: " + skillPilotLevel.getText().toString()
+                + "\nFighter Skill: " + skillFighterLevel.getText().toString()
+                + "\nEngineer Skill: " + skillEngineerLevel.getText().toString()
+                + "\nTrader Skill: " + skillTraderLevel.getText().toString();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, arraySpinner);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s.setAdapter(adapter);
+
+        Button createButton = (Button) findViewById(R.id.createButton);
+        createButton.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                Log.w(TAG, playerInformation);
+            }
+        });
     }
 
 }
