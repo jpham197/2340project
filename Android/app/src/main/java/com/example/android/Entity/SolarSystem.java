856498a7aebd568e5a04fb43ca3aadfec04a5e8a
Noah Le <noahle@lawn-143-215-60-227.lawn.gatech.edu>
2019-02-27 20:31:53 -0500

    Added X and Y coordinates to the Solar System class, and changed the constructor as well. Modified the Universe Class by: adding a constructor, private instance of SolarSystem[], and then some methods to retrieve those variables.

--- Android/app/src/main/java/com/example/android/Entity/SolarSystem.java
+++ Android/app/src/main/java/com/example/android/Entity/SolarSystem.java
@@ -6,17 +6,33 @@ public class SolarSystem {
     private TechLevel techLevel;
     private int numOfPlanets;
     private Planet[] planets;
+    private int Xcoordinate;
+    private int Ycoordinate;
 
 
-    SolarSystem(String name, TechLevel techLevel, int numOfPlanets, Planet[] planets) {
+    SolarSystem(String name, TechLevel techLevel, int numOfPlanets, Planet[] planets
+        , int Xcoordinate, int Ycoordinate) {
+
         this.name = name;
         this.techLevel = techLevel;
         this.numOfPlanets = numOfPlanets;
         this.planets = planets;
+        this.Xcoordinate = Xcoordinate;
+        this.Ycoordinate = Ycoordinate;
     }
 
+    /**
+     * Default values if not entered has:
+     *          Array of Planets
+     *          Xcoordinate should be between 0 - 100
+     *          Ycoordinate should be between 0 - 100
+     * @param name
+     * @param techLevel
+     * @param numOfPlanets
+     */
     SolarSystem(String name, TechLevel techLevel, int numOfPlanets){
-        this(name, techLevel, numOfPlanets, new Planet[numOfPlanets]);
+        this(name, techLevel, numOfPlanets, new Planet[numOfPlanets]
+                ,(int) Math.random() * 100, (int) Math.random() * 100);
     }
 
 }
