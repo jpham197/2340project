38a232d188b84e62b95e318d5f11eb9ca22ddba1
samuel chen <schen634@gatech.edu>
2019-02-28 22:05:02 -0500

    Added EditAddPlayerViewModel

---
+++ Android/app/src/main/java/com/example/android/ViewModels/EditAddPlayerViewModel.java
@@ -0,0 +1,22 @@
+package com.example.android.ViewModels;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.support.annotation.NonNull;
+
+import com.example.android.Entity.Player;
+import com.example.android.Model.PlayerInteractor;
+import com.example.android.Model.Model;
+
+public class EditAddPlayerViewModel extends AndroidViewModel {
+    private PlayerInteractor interactor;
+
+    public EditAddPlayerViewModel(@NonNull Application application) {
+        super(application);
+        interactor = Model.getInstance().getPlayerInteractor();
+    }
+
+    public void updatePlayer(Player player) { interactor.updatePlayer(player); }
+
+    public void addPlayer(Player player) { interactor.addPlayer(player); }
+}
