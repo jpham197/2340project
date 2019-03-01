20183cc57ffbecb626c9d665872a76f21994b8de
jpham197 <jpham197@gmail.com>
2019-02-28 22:53:25 -0500

    Created PlayerViewModel

--- Android/app/src/main/java/com/example/android/ViewModels/PlayerViewModel.java
+++ Android/app/src/main/java/com/example/android/ViewModels/PlayerViewModel.java
@@ -1,4 +1,36 @@
 package com.example.android.ViewModels;
 
-public class PlayerViewModel {
+import android.app.Application;
+import android.support.annotation.NonNull;
+import android.arch.lifecycle.AndroidViewModel;
+
+import com.example.android.Entity.Player;
+import com.example.android.Entity.Ship;
+import com.example.android.Model.PlayerInteractor;
+
+import java.util.List;
+
+public class PlayerViewModel extends AndroidViewModel {
+    private PlayerInteractor model;
+    private List<Player> players;
+
+    public PlayerViewModel(@NonNull Application application) {
+        super(application);
+        model = Model.getInstance().getPlayerInteractor();
+        players = model.getAllPlayers();
+    }
+
+    public void addPlayer(String name, int pilot, int fighter, int trader, int engineer
+            , int skillPointsAvailable, int credits, Ship current) {
+        Player player = new Player(name, pilot, fighter, trader, engineer
+                  , skillPointsAvailable, credits, current);
+        players.add(player);
+        players = model.getAllPlayers();
+    }
+
+    public void deletePlayer(Player player) {
+        model.deletePlayer(player);
+    }
+
+
 }
