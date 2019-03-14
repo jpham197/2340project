2e774c41322189c60070b375adadf0cd3e8f4859
jpham197 <jpham197@gmail.com>
2019-03-13 21:49:22 -0400

    Created add and remove methods for market

--- Android/app/src/main/java/com/example/android/Entity/Market.java
+++ Android/app/src/main/java/com/example/android/Entity/Market.java
@@ -1,5 +1,44 @@
 package com.example.android.Entity;
 
-public class Market {
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
 
+public class Market {
+    //MapEntry is <stock, price>
+    HashMap<String, Map.Entry<Integer, Integer>> storage;
+
+
+    public Market() {
+        storage = new HashMap<>();
+    }
+
+    //To do: Make constructor to create market from collection
+    //public Market(Collection)
+
+    /**
+     * Method within market for market interactor to use in its buy()
+     *
+     * @param item
+     * @return the item removed
+     */
+    public Map.Entry<Integer, Integer> removeItem(String item) {
+        if (storage.containsKey(item)) {
+            return storage.remove(item);
+        } else {
+            throw new NoSuchElementException("Item not in storage");
+        }
+    }
+
+    /**
+     * Method within market for market interactor to use in its sell()
+     * @param item the item being added
+     */
+    public void addItem(String item) {
+        Map.Entry<Integer,Integer> itemObject = storage.get(item);
+        Integer stock = itemObject.getKey();
+        Integer price = itemObject.getValue();
+        storage.put(item, Map.Entry<stock, price>);
+    }
 }
