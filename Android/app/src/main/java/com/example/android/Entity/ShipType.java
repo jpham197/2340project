f545e03cd4908941c17fff04d25da0d5a7c8adff
jpham197 <jpham197@gmail.com>
2019-03-14 17:39:53 -0400

    Added inventory to ship

--- Android/app/src/main/java/com/example/android/Entity/ShipType.java
+++ Android/app/src/main/java/com/example/android/Entity/ShipType.java
@@ -1,7 +1,15 @@
 package com.example.android.Entity;
 
 public enum ShipType {
-    Gnat,
-    Beetle,
-    Dragonfly
+    Gnat(10),
+    Beetle(20),
+    Dragonfly(30);
+
+    private final int inventorySize;
+    private final Item[] inventory;
+
+    ShipType(int inventorySize) {
+        this.inventorySize = inventorySize;
+        inventory = new Item[inventorySize];
+    }
 }
