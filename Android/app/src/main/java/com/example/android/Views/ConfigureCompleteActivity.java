268919e86b2b4efe5c0bd5d7adccd6495258255a
samuel chen <schen634@gatech.edu>
2019-03-14 22:29:12 -0400

    filled out marketplacestartup.

--- Android/app/src/main/java/com/example/android/Views/ConfigureCompleteActivity.java
+++ Android/app/src/main/java/com/example/android/Views/ConfigureCompleteActivity.java
@@ -1,5 +1,7 @@
 package com.example.android.Views;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,11 +11,18 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.android.Entity.Location;
+import com.example.android.Entity.Planet;
+import com.example.android.Entity.Player;
+import com.example.android.Entity.SolarSystem;
+import com.example.android.Model.Repository;
 import com.example.android.R;
+
+import java.util.List;
 
 public class ConfigureCompleteActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
-
+    Repository repo;
 
 //    final String playerPilotSkill = getIntent().getStringExtra("playerPilotSkill");
 //    final String playerFighterSkill = getIntent().getStringExtra("playerFighterSkill");
@@ -25,19 +34,33 @@ public class ConfigureCompleteActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configure_complete);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         final String playerName = getIntent().getExtras().getString("playerName");
         setTitle("Space Traders: " + playerName);
-
+        repo = new Repository();
         Button trade = findViewById(R.id.PlanetTradeButton);
         trade.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.w(TAG, "TEST");
                 Log.w(TAG, playerName);
+                Intent intent = new Intent (ConfigureCompleteActivity.this, MarketplaceStartup.class);
+                List list = repo.getAllSolarSystems();
+                SolarSystem solarSystem = (SolarSystem) list.get((int)Math.random() * list.size());
+                Planet[] planets = solarSystem.getPlanets();
+                Planet planet = planets[0];
+                int x = planet.getxCoordinate();
+                int y = planet.getyCoordinate();
+                Location location = new Location(x, y);
+                List players = repo.getAllPlayers();
+                Player player = (Player) players.get(0);
+                player.setLocation(location);
+                repo.setCurrentPlanet(planet);
+                startActivity(intent);
             }
         });
     }
