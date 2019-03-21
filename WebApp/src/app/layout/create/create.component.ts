557e9194934cdb5e4616742bdbd0b6d505b01ce1
Jimmy <jpham197@gmail.com>
2019-03-20 20:49:41 -0400

    skill point allocation logic

--- WebApp/src/app/layout/create/create.component.ts
+++ WebApp/src/app/layout/create/create.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateComponent implements OnInit {
 
+  availablePoints = 16;
+  test1 = 0;
+  
   constructor() {  }
 
   ngOnInit() {
+  }
+
+  plusFighter() {
+    if (this.availablePoints != 0) {
+      this.test1++;
+      this.availablePoints--;
+    }
+  }
+
+  minusFighter() {
+    if (this.test1 > 0) {
+      this.test1--;
+      this.availablePoints++;
+    }
   }
 
 }
