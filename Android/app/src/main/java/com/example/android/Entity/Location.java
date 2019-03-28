82b4bcd9ce624ea60927da30b306cdf241c396c0
jpham197 <jpham197@gmail.com>
2019-03-28 19:11:29 -0400

    Finished getters and setters and created calculate distance method for travel

--- Android/app/src/main/java/com/example/android/Entity/Location.java
+++ Android/app/src/main/java/com/example/android/Entity/Location.java
@@ -9,13 +9,27 @@ public class Location {
         this.y = y;
     }
 
-    public void setX() {
+    public int getX() {
+        return x;
+    }
 
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
     }
 
 
-    public void setY() {
+    public void setY(int y) {
+        this.y = y;
+    }
 
+    public int calcDistance(Location LOne, Location LTwo) {
+        int distX = (LOne.getX() - LTwo.getX()) * (LOne.getX() - LTwo.getX());
+        int distY = (LOne.getY() - LTwo.getY()) * (LOne.getY() - LTwo.getY());
+        return (int) Math.floor(Math.sqrt(distX + distY));
     }
 
     @Override
